8c4122179b73919d5668ddda3e773302bcd1c9f1
Seunghun Lee <waydi1@gmail.com>
2017-06-09 18:19:53 +0900

    Add ToDoItem

---
+++ ToDo/Model/ToDoItem.swift
@@ -0,0 +1,13 @@
+//
+//  ToDoItem.swift
+//  ToDo
+//
+//  Created by Seunghun Lee on 2017-06-09.
+//  Copyright © 2017 Seunghun Lee. All rights reserved.
+//
+
+import Foundation
+
+struct ToDoItem {
+    var title: String
+}
